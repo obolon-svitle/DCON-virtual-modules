c3265622f70a5cc3dfb6db252129aa7f67b36fa4
Andrey Chernyavsky <chernyav.a@gmail.com>
2016-04-17 13:42:44 +0300

    Refactor source code of the 7017 module

--- dcon/devices/7017/7017.c
+++ dcon/devices/7017/7017.c
@@ -3,19 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "stellaris/hw_types.h"
-#include "stellaris/hw_memmap.h"
-#include "stellaris/hw_ints.h"
-#include "stellaris/driverlib/sysctl.h"
-#include "stellaris/driverlib/adc.h"
-#include "stellaris/driverlib/gpio.h"
-#include "stellaris/driverlib/interrupt.h"
-
 #include <common.h>
 
 #include <dcon/dcon_dev.h>
 #include "dcon/dcon_dev_common.h"
 #include "7017.h"
+#include "7017_hw.h"
 
 #define MODULE_7017_ADDR 0x02
 #define MODULE_7017_TYPE 0x0A
@@ -45,10 +38,6 @@ static struct dcon_dev dev_7017 = {
     .addr = MODULE_7017_ADDR,
     .type = MODULE_7017_TYPE,
 };
-
-#define ADC_DIVIDER 170.5
-#define MAX_VOLTAGE 5
-#define ADC_CHANNEL_COUNT 8
 
 static SemaphoreHandle_t adc_mutex;
 static unsigned long adc_vals[ADC_CHANNEL_COUNT];
@@ -106,53 +95,11 @@ static void get_7017_adc_channel_data(const char *request, char *response) {
     snprintf(response, DCON_MAX_BUF, ">%+2.4ld\r", adc_vals[(unsigned)request[3]]);
 }
 
-void ADCIntHandler(void) {
-    unsigned long adc_vals[ADC_CHANNEL_COUNT];
-
-    ADCIntClear(ADC0_BASE, 0);
-
-    xSemaphoreTakeFromISR(adc_mutex, NULL);
-    ADCSequenceDataGet(ADC0_BASE, 0, &(adc_vals[0]));
-    xSemaphoreGiveFromISR(adc_mutex, NULL);
-}
-
-static void init_ADC(void) {
-    unsigned long ulDummy = 0;
-
-    adc_mutex = xSemaphoreCreateMutex();
-    if (adc_mutex == NULL) {
-        return;
-    }        
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    GPIOPinTypeADC(GPIO_PORTC_BASE,
-                   GPIO_PIN_7| GPIO_PIN_6| GPIO_PIN_5| GPIO_PIN_4);
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_1 | GPIO_PIN_2);
-
-    ADCSequenceDisable(ADC0_BASE, 0);
-    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
-
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 1, ADC_CTL_CH11);
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 2, ADC_CTL_CH4);                   
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 3, ADC_CTL_CH5);                   
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 4, ADC_CTL_CH6);                   
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 5, ADC_CTL_CH7 | ADC_CTL_IE | ADC_CTL_END);
-    ADCSequenceEnable(ADC0_BASE, 0);
-    
-    ADCSequenceDataGet(ADC0_BASE, 0, &ulDummy);
-
-    ADCIntEnable(ADC0_BASE, 0);
-    IntEnable(INT_ADC0);
-}
-
-
 void Task7017Function(void *pvParameters){
     struct msg msg;
     int result;
 
-    init_ADC();
+    dev_init();
 
     dcon_dev_register(&dev_7017);
 
---
+++ dcon/devices/7017/7017_hw.h
@@ -0,0 +1,10 @@
+#ifndef _7017_HW_H_
+#define _7017_HW_H_
+
+#define ADC_DIVIDER 170.5
+#define MAX_VOLTAGE 5
+#define ADC_CHANNEL_COUNT 8
+
+void dev_init(void);
+
+#endif /*_7017_HW_H */
