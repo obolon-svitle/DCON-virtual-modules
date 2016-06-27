32c123ffd10e6d78e7a7ec17e38fc3fc09d878b6
Andrii Chernyavskyi <a.cherniavskiy@sirinsoftware.com>
2016-06-27 16:40:19 +0000

    Refactored 7017 code

--- src/dcon/devices/7017/7017_hw.c
+++ src/dcon/devices/7017/7017_hw.c
@@ -17,14 +17,23 @@
 #define MAX_VOLTAGE 3.3
 #define RESOLUTION 1024
 
+#define MODULE_ADC_PERIPH SYSCTL_PERIPH_ADC0
+#define MODULE_ADC_BASE ADC0_BASE
+#define MODULE_ADC_SEQUENCE_NUM 3
+#define MODULE_ADC_SEQUENCE_0_CONFIG (ADC_CTL_CH4)
+#define MODULE_ADC_SEQUENCE_1_CONFIG (ADC_CTL_CH5 | ADC_CTL_IE | ADC_CTL_END)
+
+#define MODULE_CONFIG_GPIO() do { SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); \
+	GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_6 | GPIO_PIN_7); } while (0)
+
 static unsigned long adc_vals[CHANNEL_COUNT];
 static SemaphoreHandle_t adc_mutex;
 
 void ADCIntHandler(void) {
-    while(!ADCIntStatus(ADC0_BASE, 3, false));
-    ADCIntClear(ADC0_BASE, 3);
-    ADCSequenceDataGet(ADC0_BASE, 3, adc_vals);
-    ADCProcessorTrigger(ADC0_BASE, 3);
+    while(!ADCIntStatus(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, false));
+    ADCIntClear(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
+    ADCSequenceDataGet(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, adc_vals);
+    ADCProcessorTrigger(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
 }
 
 void get_voltage(int channel, int *intpart, int *floatpart) {
@@ -40,22 +49,20 @@ void init_7017(void) {
         return;
     }        
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-    GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_6 | GPIO_PIN_7);
+    SysCtlPeripheralEnable(MODULE_ADC_PERIPH);
+
+    MODULE_CONFIG_GPIO();
+
+    ADCSequenceDisable(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
+    ADCSequenceConfigure(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, ADC_TRIGGER_PROCESSOR, 0);
+
+    ADCSequenceStepConfigure(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, 0, MODULE_ADC_SEQUENCE_1_CONFIG);
+    ADCSequenceStepConfigure(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, 1, MODULE_ADC_SEQUENCE_1_CONFIG);
+    ADCIntRegister(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM, ADCIntHandler);
     
-    ADCSequenceDisable(ADC0_BASE, 3);
-    ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_PROCESSOR, 0);
+    ADCIntEnable(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
+    ADCSequenceEnable(ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
+    ADCIntClear(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
 
-    ADCSequenceStepConfigure(ADC0_BASE, 3, 0, ADC_CTL_CH4);
-    ADCSequenceStepConfigure(ADC0_BASE, 3, 1, ADC_CTL_CH5 | ADC_CTL_IE | ADC_CTL_END);
-    ADCIntRegister(ADC0_BASE, 3, ADCIntHandler);
-    
-    ADCIntEnable(ADC0_BASE, 3);
-    ADCSequenceEnable(ADC_BASE, 3);
-    ADCIntClear(ADC0_BASE, 3);
-
-    ADCProcessorTrigger(ADC0_BASE, 3);
-
-
+    ADCProcessorTrigger(MODULE_ADC_BASE, MODULE_ADC_SEQUENCE_NUM);
 }
